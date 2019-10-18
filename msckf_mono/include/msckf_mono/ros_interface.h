c178ee7b71b4aeff063079d6cc03c9c9299dee4c
cggos <cggos@outlook.com>
2019-10-18 15:46:19 +0800

    format ...

--- msckf_mono/include/msckf_mono/ros_interface.h
+++ msckf_mono/include/msckf_mono/ros_interface.h
@@ -14,75 +14,81 @@
 #include <msckf_mono/corner_detector.h>
 #include <atomic>
 
-namespace msckf_mono
-{
-  class RosInterface {
+namespace msckf_mono {
+    class RosInterface {
     public:
-      EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+        EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
-      RosInterface(ros::NodeHandle nh);
+        RosInterface(ros::NodeHandle nh);
 
-      void imuCallback(const sensor_msgs::ImuConstPtr& imu);
+        void imuCallback(const sensor_msgs::ImuConstPtr &imu);
 
-      void imageCallback(const sensor_msgs::ImageConstPtr& msg);
+        void imageCallback(const sensor_msgs::ImageConstPtr &msg);
 
-      void publish_core(const ros::Time& publish_time);
+        void publish_core(const ros::Time &publish_time);
 
-      void publish_extra(const ros::Time& publish_time);
+        void publish_extra(const ros::Time &publish_time);
 
     private:
-      ros::NodeHandle nh_;
-      image_transport::ImageTransport it_;
+        ros::NodeHandle nh_;
+        image_transport::ImageTransport it_;
 
-      image_transport::Subscriber image_sub_;
-      image_transport::Publisher track_image_pub_;
-      ros::Publisher odom_pub_;
+        image_transport::Subscriber image_sub_;
+        image_transport::Publisher track_image_pub_;
+        ros::Publisher odom_pub_;
 
-      ros::Subscriber imu_sub_;
+        ros::Subscriber imu_sub_;
 
-      void load_parameters();
+        void load_parameters();
 
-      bool debug_;
+        bool debug_;
 
-      std::vector<std::tuple<double, imuReading<float>>> imu_queue_;
-      double prev_imu_time_;
+        std::vector<std::tuple<double, imuReading<float>>> imu_queue_;
+        double prev_imu_time_;
 
-      void setup_track_handler();
-      std::shared_ptr<corner_detector::TrackHandler> track_handler_;
+        void setup_track_handler();
 
-      Matrix3<float> R_imu_cam_;
-      Vector3<float> p_imu_cam_;
+        std::shared_ptr<corner_detector::TrackHandler> track_handler_;
 
-      Matrix3<float> R_cam_imu_;
-      Vector3<float> p_cam_imu_;
+        Matrix3<float> R_imu_cam_;
+        Vector3<float> p_imu_cam_;
 
-      std::string camera_model_;
-      cv::Mat K_;
-      std::string distortion_model_;
-      cv::Mat dist_coeffs_;
+        Matrix3<float> R_cam_imu_;
+        Vector3<float> p_cam_imu_;
 
-      int n_grid_cols_;
-      int n_grid_rows_;
-      float ransac_threshold_;
+        std::string camera_model_;
+        cv::Mat K_;
+        std::string distortion_model_;
+        cv::Mat dist_coeffs_;
 
-      enum CalibrationMethod { TimedStandStill };
-      CalibrationMethod imu_calibration_method_;
+        int n_grid_cols_;
+        int n_grid_rows_;
+        float ransac_threshold_;
 
-      double stand_still_time_;
-      double done_stand_still_time_;
+        enum CalibrationMethod {
+            TimedStandStill
+        };
+        CalibrationMethod imu_calibration_method_;
 
-      std::atomic<bool> imu_calibrated_;
-      bool can_initialize_imu();
-      void initialize_imu();
+        double stand_still_time_;
+        double done_stand_still_time_;
 
-      int state_k_;
-      void setup_msckf();
-      MSCKF<float> msckf_;
-      Camera<float> camera_;
-      noiseParams<float> noise_params_;
-      MSCKFParams<float> msckf_params_;
-      imuState<float> init_imu_state_;
-  };
+        std::atomic<bool> imu_calibrated_;
+
+        bool can_initialize_imu();
+
+        void initialize_imu();
+
+        int state_k_;
+
+        void setup_msckf();
+
+        MSCKF<float> msckf_;
+        Camera<float> camera_;
+        noiseParams<float> noise_params_;
+        MSCKFParams<float> msckf_params_;
+        imuState<float> init_imu_state_;
+    };
 }
 
 #endif
--- msckf_mono/nodes/msckf_mono_node.cpp
+++ msckf_mono/nodes/msckf_mono_node.cpp
@@ -2,8 +2,8 @@
 #include <msckf_mono/ros_interface.h>
 
 int main(int argc, char** argv) {
-  ros::init(argc, argv, "msckf_mono_node");
-  ros::NodeHandle nh;
-  msckf_mono::RosInterface ri(nh);
-  ros::spin();
+    ros::init(argc, argv, "msckf_mono_node");
+    ros::NodeHandle nh;
+    msckf_mono::RosInterface ri(nh);
+    ros::spin();
 }
