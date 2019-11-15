383c6c0d60d5d0d8b87b351a7f2984451764f48d
cggos <cggos@outlook.com>
2019-11-15 18:30:28 +0800

    update msckf_vio/src/msckf_vio.cpp

--- msckf_vio/include/msckf_vio/imu_state.h
+++ msckf_vio/include/msckf_vio/imu_state.h
@@ -17,88 +17,85 @@
 
 namespace msckf_vio {
 
-/*
- * @brief IMUState State for IMU
- */
-struct IMUState {
-  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-  typedef long long int StateIDType;
+    /**
+     * @brief IMUState State for IMU
+     */
+    struct IMUState {
+        EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
-  // An unique identifier for the IMU state.
-  StateIDType id;
+        typedef long long int StateIDType;
 
-  // id for next IMU state
-  static StateIDType next_id;
+        // An unique identifier for the IMU state.
+        StateIDType id;
 
-  // Time when the state is recorded
-  double time;
+        // id for next IMU state
+        static StateIDType next_id;
 
-  // Orientation q_I_G
-  // Take a vector from the world frame to the IMU (body) frame.
-  Eigen::Vector4d orientation;
+        // Time when the state is recorded
+        double time;
 
-  // Position of the IMU (body) frame in the world frame.
-  Eigen::Vector3d position;
+        // Orientation q_I_G
+        // Take a vector from the world frame to the IMU (body) frame.
+        Eigen::Vector4d orientation;
 
-  // Velocity of the IMU (body) frame in the world frame.
-  Eigen::Vector3d velocity;
+        // Position of the IMU (body) frame in the world frame.
+        Eigen::Vector3d position;
 
-  // Bias for measured angular velocity and acceleration.
-  Eigen::Vector3d gyro_bias;
-  Eigen::Vector3d acc_bias;
+        // Velocity of the IMU (body) frame in the world frame.
+        Eigen::Vector3d velocity;
 
-  // Transformation between the IMU and the left camera (cam0)
-  Eigen::Matrix3d R_imu_cam0;
-  Eigen::Vector3d t_cam0_imu;
+        // Bias for measured angular velocity and acceleration.
+        Eigen::Vector3d gyro_bias;
+        Eigen::Vector3d acc_bias;
 
-  // These three variables should have the same physical
-  // interpretation with `orientation`, `position`, and
-  // `velocity`. There three variables are used to modify
-  // the transition matrices to make the observability matrix
-  // have proper null space.
-  Eigen::Vector4d orientation_null;
-  Eigen::Vector3d position_null;
-  Eigen::Vector3d velocity_null;
+        // Transformation between the IMU and the left camera (cam0)
+        Eigen::Matrix3d R_imu_cam0;
+        Eigen::Vector3d t_cam0_imu;
 
-  // Process noise
-  static double gyro_noise;
-  static double acc_noise;
-  static double gyro_bias_noise;
-  static double acc_bias_noise;
+        // These three variables should have the same physical interpretation with `orientation`, `position`, and `velocity`.
+        // There three variables are used to modify the transition matrices to make the observability matrix have proper null space.
+        Eigen::Vector4d orientation_null;
+        Eigen::Vector3d position_null;
+        Eigen::Vector3d velocity_null;
 
-  // Gravity vector in the world frame
-  static Eigen::Vector3d gravity;
+        // Process noise
+        static double gyro_noise;
+        static double acc_noise;
+        static double gyro_bias_noise;
+        static double acc_bias_noise;
 
-  // Transformation offset from the IMU frame to
-  // the body frame. The transformation takes a
-  // vector from the IMU frame to the body frame.
-  // The z axis of the body frame should point upwards.
-  // Normally, this transform should be identity.
-  static Eigen::Isometry3d T_imu_body;
+        // Gravity vector in the world frame
+        static Eigen::Vector3d gravity;
 
-  IMUState(): id(0), time(0),
-    orientation(Eigen::Vector4d(0, 0, 0, 1)),
-    position(Eigen::Vector3d::Zero()),
-    velocity(Eigen::Vector3d::Zero()),
-    gyro_bias(Eigen::Vector3d::Zero()),
-    acc_bias(Eigen::Vector3d::Zero()),
-    orientation_null(Eigen::Vector4d(0, 0, 0, 1)),
-    position_null(Eigen::Vector3d::Zero()),
-    velocity_null(Eigen::Vector3d::Zero()) {}
+        // Transformation offset from the IMU frame to the body frame.
+        // The transformation takes a vector from the IMU frame to the body frame.
+        // The z axis of the body frame should point upwards.
+        // Normally, this transform should be identity.
+        static Eigen::Isometry3d T_imu_body;
 
-  IMUState(const StateIDType& new_id): id(new_id), time(0),
-    orientation(Eigen::Vector4d(0, 0, 0, 1)),
-    position(Eigen::Vector3d::Zero()),
-    velocity(Eigen::Vector3d::Zero()),
-    gyro_bias(Eigen::Vector3d::Zero()),
-    acc_bias(Eigen::Vector3d::Zero()),
-    orientation_null(Eigen::Vector4d(0, 0, 0, 1)),
-    position_null(Eigen::Vector3d::Zero()),
-    velocity_null(Eigen::Vector3d::Zero()) {}
+        IMUState() : id(0), time(0),
+                     orientation(Eigen::Vector4d(0, 0, 0, 1)),
+                     position(Eigen::Vector3d::Zero()),
+                     velocity(Eigen::Vector3d::Zero()),
+                     gyro_bias(Eigen::Vector3d::Zero()),
+                     acc_bias(Eigen::Vector3d::Zero()),
+                     orientation_null(Eigen::Vector4d(0, 0, 0, 1)),
+                     position_null(Eigen::Vector3d::Zero()),
+                     velocity_null(Eigen::Vector3d::Zero()) {}
 
-};
+        IMUState(const StateIDType &new_id) : id(new_id), time(0),
+                                              orientation(Eigen::Vector4d(0, 0, 0, 1)),
+                                              position(Eigen::Vector3d::Zero()),
+                                              velocity(Eigen::Vector3d::Zero()),
+                                              gyro_bias(Eigen::Vector3d::Zero()),
+                                              acc_bias(Eigen::Vector3d::Zero()),
+                                              orientation_null(Eigen::Vector4d(0, 0, 0, 1)),
+                                              position_null(Eigen::Vector3d::Zero()),
+                                              velocity_null(Eigen::Vector3d::Zero()) {}
 
-typedef IMUState::StateIDType StateIDType;
+    };
+
+    typedef IMUState::StateIDType StateIDType;
 
 } // namespace msckf_vio
 
--- msckf_vio/include/msckf_vio/msckf_vio.h
+++ msckf_vio/include/msckf_vio/msckf_vio.h
@@ -31,214 +31,235 @@
 #include <msckf_vio/CameraMeasurement.h>
 
 namespace msckf_vio {
-/*
- * @brief MsckfVio Implements the algorithm in
- *    Anatasios I. Mourikis, and Stergios I. Roumeliotis,
- *    "A Multi-State Constraint Kalman Filter for Vision-aided
- *    Inertial Navigation",
- *    http://www.ee.ucr.edu/~mourikis/tech_reports/TR_MSCKF.pdf
- */
-class MsckfVio {
-  public:
-    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-
-    // Constructor
-    MsckfVio(ros::NodeHandle& pnh);
-    // Disable copy and assign constructor
-    MsckfVio(const MsckfVio&) = delete;
-    MsckfVio operator=(const MsckfVio&) = delete;
-
-    // Destructor
-    ~MsckfVio() {
-        pose_outfile_.close();
-    }
-
-    /*
-     * @brief initialize Initialize the VIO.
+    /**
+     * @brief MsckfVio Implements the algorithm in
+     *    Anatasios I. Mourikis, and Stergios I. Roumeliotis,
+     *    "A Multi-State Constraint Kalman Filter for Vision-aided Inertial Navigation",
+     *    http://www.ee.ucr.edu/~mourikis/tech_reports/TR_MSCKF.pdf
      */
-    bool initialize();
+    class MsckfVio {
+    public:
+        EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
-    /*
-     * @brief reset Resets the VIO to initial status.
-     */
-    void reset();
+        // Constructor
+        MsckfVio(ros::NodeHandle &pnh);
 
-    typedef boost::shared_ptr<MsckfVio> Ptr;
-    typedef boost::shared_ptr<const MsckfVio> ConstPtr;
+        // Disable copy and assign constructor
+        MsckfVio(const MsckfVio &) = delete;
 
-  private:
-    /*
-     * @brief StateServer Store one IMU states and several camera states for constructing measurement model.
-     */
-    struct StateServer {
-      IMUState imu_state;
-      CamStateServer cam_states;
+        MsckfVio operator=(const MsckfVio &) = delete;
 
-      // State covariance matrix
-      Eigen::MatrixXd state_cov;
-      Eigen::Matrix<double, 12, 12> continuous_noise_cov;
+        // Destructor
+        ~MsckfVio() {
+            pose_outfile_.close();
+        }
+
+        /**
+         * @brief initialize Initialize the VIO.
+         */
+        bool initialize();
+
+        /**
+         * @brief reset Resets the VIO to initial status.
+         */
+        void reset();
+
+        typedef boost::shared_ptr<MsckfVio> Ptr;
+        typedef boost::shared_ptr<const MsckfVio> ConstPtr;
+
+    private:
+        /**
+         * @brief StateServer Store one IMU states and several camera states for constructing measurement model.
+         */
+        struct StateServer {
+            IMUState imu_state;
+            CamStateServer cam_states;
+
+            // State covariance matrix
+            Eigen::MatrixXd state_cov;
+            Eigen::Matrix<double, 12, 12> continuous_noise_cov;
+        };
+
+
+        /**
+         * @brief loadParameters Load parameters from the parameter server.
+         */
+        bool loadParameters();
+
+        /**
+         * @brief createRosIO Create ros publisher and subscirbers.
+         */
+        bool createRosIO();
+
+        /**
+         * @brief imuCallback Callback function for the imu message.
+         * @details 接收IMU数据，将IMU数据存到imu_msg_buffer中，这里只会利用开头200帧IMU数据进行静止初始化，不做其他处理
+         * @param msg IMU msg.
+         */
+        void imuCallback(const sensor_msgs::ImuConstPtr &msg);
+
+        /**
+         * @brief featureCallback Callback function for feature measurements.
+         * @details 接收双目特征，进行后端处理。利用IMU进行EKF Propagation，利用双目特征进行EKF Update
+         * @param msg Stereo feature measurements.
+         */
+        void featureCallback(const CameraMeasurementConstPtr &msg);
+
+        /**
+         * @brief publish Publish the results of VIO.
+         * @param time The time stamp of output msgs.
+         */
+        void publish(const ros::Time &time);
+
+        /**
+         * @brief initializegravityAndBias
+         *    Initialize the IMU bias and initial orientation based on the first few IMU readings.
+         * @details 将前200帧加速度和角速度求平均, 平均加速度的模值g作为重力加速度, 平均角速度作为陀螺仪的bias,
+         *          计算重力向量(0,0,-g)和平均加速度之间的夹角(旋转四元数), 标定初始时刻IMU系与world系之间的夹角.
+         *          因此MSCKF要求前200帧IMU是静止不动的
+         */
+        void initializeGravityAndBias();
+
+        /**
+         * @biref resetCallback
+         *    Callback function for the reset service.
+         *    Note that this is NOT anytime-reset.
+         *    This function should only be called before the sensor suite starts moving.
+         *    e.g. while the robot is still on the ground.
+         */
+        bool resetCallback(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res);
+
+        /**
+         * @brief Filter related functions Propogate the state
+         * @param time_bound
+         */
+        void batchImuProcessing(const double &time_bound);
+
+        /**
+         * @brief 单帧IMU过程模型：状态向量预测、状态协方差预测
+         * @param time
+         * @param m_gyro
+         * @param m_acc
+         */
+        void processModel(const double &time, const Eigen::Vector3d &m_gyro, const Eigen::Vector3d &m_acc);
+
+        // 状态向量预测
+        void predictNewState(const double &dt, const Eigen::Vector3d &gyro, const Eigen::Vector3d &acc);
+
+        // Measurement update
+        void stateAugmentation(const double &time);
+
+        /**
+         * @brief Add new observations for existing features or new features in the map server.
+         * @param msg
+         */
+        void addFeatureObservations(const CameraMeasurementConstPtr &msg);
+
+        // This function is used to compute the measurement Jacobian for a single feature observed at a single camera frame.
+        void measurementJacobian(const StateIDType &cam_state_id, const FeatureIDType &feature_id,
+                                 Eigen::Matrix<double, 4, 6> &H_x, Eigen::Matrix<double, 4, 3> &H_f, Eigen::Vector4d &r);
+
+        // This function computes the Jacobian of all measurements viewed in the given camera states of this feature.
+        void featureJacobian(const FeatureIDType &feature_id, const std::vector<StateIDType> &cam_state_ids,
+                             Eigen::MatrixXd &H_x, Eigen::VectorXd &r);
+
+        /**
+         * @details 在两种情况下触发：
+         *           1）特征跟丢了需要移除特征时（removeLostFeatures）
+         *           2）相机状态数量达到最大限制需要剔除掉相机状态时（pruneCamStateBuffer）
+         * @param H
+         * @param r
+         */
+        void measurementUpdate(const Eigen::MatrixXd &H, const Eigen::VectorXd &r);
+
+        bool gatingTest(const Eigen::MatrixXd &H, const Eigen::VectorXd &r, const int &dof);
+
+        void removeLostFeatures();
+
+        void findRedundantCamStates(std::vector<StateIDType> &rm_cam_state_ids);
+
+        void pruneCamStateBuffer();
+
+        // Reset the system online if the uncertainty is too large.
+        void onlineReset();
+
+        // Chi squared test table.
+        static std::map<int, double> chi_squared_test_table;
+
+        // State vector
+        StateServer state_server;
+        // Maximum number of camera states
+        int max_cam_state_size;
+
+        // Features used
+        MapServer map_server;
+
+        // IMU data buffer
+        // This is buffer is used to handle the unsynchronization or
+        // transfer delay between IMU and Image messages.
+        std::vector<sensor_msgs::Imu> imu_msg_buffer;
+
+        // Indicate if the gravity vector is set.
+        bool is_gravity_set;
+
+        // Indicate if the received image is the first one. The
+        // system will start after receiving the first image.
+        bool is_first_img;
+
+        // The position uncertainty threshold is used to determine
+        // when to reset the system online. Otherwise, the ever-
+        // increaseing uncertainty will make the estimation unstable.
+        // Note this online reset will be some dead-reckoning.
+        // Set this threshold to nonpositive to disable online reset.
+        double position_std_threshold;
+
+        // Tracking rate
+        double tracking_rate;
+
+        // Threshold for determine keyframes
+        double translation_threshold;
+        double rotation_threshold;
+        double tracking_rate_threshold;
+
+        // Ros node handle
+        ros::NodeHandle nh;
+
+        // Subscribers and publishers
+        ros::Subscriber imu_sub;
+        ros::Subscriber feature_sub;
+        ros::Publisher odom_pub;
+        ros::Publisher path_pub;
+        ros::Publisher feature_pub;
+        tf::TransformBroadcaster tf_pub;
+        ros::ServiceServer reset_srv;
+
+        nav_msgs::Path path_msg;
+
+        // Frame id
+        std::string fixed_frame_id;
+        std::string child_frame_id;
+
+        // Whether to publish tf or not.
+        bool publish_tf;
+
+        // Framte rate of the stereo images. This variable is
+        // only used to determine the timing threshold of
+        // each iteration of the filter.
+        double frame_rate;
+
+        // Debugging variables and functions
+        void mocapOdomCallback(const nav_msgs::OdometryConstPtr &msg);
+
+        ros::Subscriber mocap_odom_sub;
+        ros::Publisher mocap_odom_pub;
+        geometry_msgs::TransformStamped raw_mocap_odom_msg;
+        Eigen::Isometry3d mocap_initial_frame;
+
+        std::ofstream pose_outfile_;
     };
 
-
-    /*
-     * @brief loadParameters Load parameters from the parameter server.
-     */
-    bool loadParameters();
-
-    /*
-     * @brief createRosIO Create ros publisher and subscirbers.
-     */
-    bool createRosIO();
-
-    /**
-     * @brief imuCallback Callback function for the imu message.
-     * @details 接收IMU数据，将IMU数据存到imu_msg_buffer中，这里只会利用开头200帧IMU数据进行静止初始化，不做其他处理
-     * @param msg IMU msg.
-     */
-    void imuCallback(const sensor_msgs::ImuConstPtr& msg);
-
-    /**
-     * @brief featureCallback Callback function for feature measurements.
-     * @details 接收双目特征，进行后端处理。利用IMU进行EKF Propagation，利用双目特征进行EKF Update
-     * @param msg Stereo feature measurements.
-     */
-    void featureCallback(const CameraMeasurementConstPtr& msg);
-
-    /*
-     * @brief publish Publish the results of VIO.
-     * @param time The time stamp of output msgs.
-     */
-    void publish(const ros::Time& time);
-
-    /**
-     * @brief initializegravityAndBias
-     *    Initialize the IMU bias and initial orientation based on the first few IMU readings.
-     * @details 将前200帧加速度和角速度求平均, 平均加速度的模值g作为重力加速度, 平均角速度作为陀螺仪的bias,
-     *          计算重力向量(0,0,-g)和平均加速度之间的夹角(旋转四元数), 标定初始时刻IMU系与world系之间的夹角.
-     *          因此MSCKF要求前200帧IMU是静止不动的
-     */
-    void initializeGravityAndBias();
-
-    /**
-     * @biref resetCallback
-     *    Callback function for the reset service.
-     *    Note that this is NOT anytime-reset.
-     *    This function should only be called before the sensor suite starts moving.
-     *    e.g. while the robot is still on the ground.
-     */
-    bool resetCallback(std_srvs::Trigger::Request& req,std_srvs::Trigger::Response& res);
-
-    // Filter related functions Propogate the state
-    void batchImuProcessing(const double& time_bound);
-    /**
-     * @brief 单帧IMU过程模型：状态向量预测、状态协方差预测
-     * @param time
-     * @param m_gyro
-     * @param m_acc
-     */
-    void processModel(const double& time, const Eigen::Vector3d& m_gyro, const Eigen::Vector3d& m_acc);
-    // 状态向量预测
-    void predictNewState(const double& dt, const Eigen::Vector3d& gyro, const Eigen::Vector3d& acc);
-
-    // Measurement update
-    void stateAugmentation(const double& time);
-    void addFeatureObservations(const CameraMeasurementConstPtr& msg);
-    // This function is used to compute the measurement Jacobian for a single feature observed at a single camera frame.
-    void measurementJacobian(const StateIDType& cam_state_id,
-        const FeatureIDType& feature_id,
-        Eigen::Matrix<double, 4, 6>& H_x,
-        Eigen::Matrix<double, 4, 3>& H_f,
-        Eigen::Vector4d& r);
-    // This function computes the Jacobian of all measurements viewed
-    // in the given camera states of this feature.
-    void featureJacobian(const FeatureIDType& feature_id,
-        const std::vector<StateIDType>& cam_state_ids,
-        Eigen::MatrixXd& H_x, Eigen::VectorXd& r);
-    void measurementUpdate(const Eigen::MatrixXd& H, const Eigen::VectorXd& r);
-    bool gatingTest(const Eigen::MatrixXd& H, const Eigen::VectorXd&r, const int& dof);
-    void removeLostFeatures();
-    void findRedundantCamStates(std::vector<StateIDType>& rm_cam_state_ids);
-    void pruneCamStateBuffer();
-    // Reset the system online if the uncertainty is too large.
-    void onlineReset();
-
-    // Chi squared test table.
-    static std::map<int, double> chi_squared_test_table;
-
-    // State vector
-    StateServer state_server;
-    // Maximum number of camera states
-    int max_cam_state_size;
-
-    // Features used
-    MapServer map_server;
-
-    // IMU data buffer
-    // This is buffer is used to handle the unsynchronization or
-    // transfer delay between IMU and Image messages.
-    std::vector<sensor_msgs::Imu> imu_msg_buffer;
-
-    // Indicate if the gravity vector is set.
-    bool is_gravity_set;
-
-    // Indicate if the received image is the first one. The
-    // system will start after receiving the first image.
-    bool is_first_img;
-
-    // The position uncertainty threshold is used to determine
-    // when to reset the system online. Otherwise, the ever-
-    // increaseing uncertainty will make the estimation unstable.
-    // Note this online reset will be some dead-reckoning.
-    // Set this threshold to nonpositive to disable online reset.
-    double position_std_threshold;
-
-    // Tracking rate
-    double tracking_rate;
-
-    // Threshold for determine keyframes
-    double translation_threshold;
-    double rotation_threshold;
-    double tracking_rate_threshold;
-
-    // Ros node handle
-    ros::NodeHandle nh;
-
-    // Subscribers and publishers
-    ros::Subscriber imu_sub;
-    ros::Subscriber feature_sub;
-    ros::Publisher odom_pub;
-    ros::Publisher path_pub;
-    ros::Publisher feature_pub;
-    tf::TransformBroadcaster tf_pub;
-    ros::ServiceServer reset_srv;
-
-    nav_msgs::Path path_msg;
-
-    // Frame id
-    std::string fixed_frame_id;
-    std::string child_frame_id;
-
-    // Whether to publish tf or not.
-    bool publish_tf;
-
-    // Framte rate of the stereo images. This variable is
-    // only used to determine the timing threshold of
-    // each iteration of the filter.
-    double frame_rate;
-
-    // Debugging variables and functions
-    void mocapOdomCallback(const nav_msgs::OdometryConstPtr& msg);
-
-    ros::Subscriber mocap_odom_sub;
-    ros::Publisher mocap_odom_pub;
-    geometry_msgs::TransformStamped raw_mocap_odom_msg;
-    Eigen::Isometry3d mocap_initial_frame;
-
-    std::ofstream pose_outfile_;
-};
-
-typedef MsckfVio::Ptr MsckfVioPtr;
-typedef MsckfVio::ConstPtr MsckfVioConstPtr;
+    typedef MsckfVio::Ptr MsckfVioPtr;
+    typedef MsckfVio::ConstPtr MsckfVioConstPtr;
 
 } // namespace msckf_vio
 
