b6d12e24c82bf9e8ceed34a4701708034f03280f
cggos <cggos@outlook.com>
2020-08-22 18:46:17 +0800

    optimize logging.h

--- include/msckf_vio/logging.h
+++ include/msckf_vio/logging.h
@@ -1,14 +1,27 @@
-#pragma once
+#ifndef CG_LOGGING_H
+#define CG_LOGGING_H
+
+#define LOG_ON true
 
 #if defined(__ANDROID__)
 
 #include <android/log.h>
-#define LOG_TAG "LARVIO"  
+
+#define LOG_TAG "MSCKF"
+
+#if LOG_ON
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO,  LOG_TAG, __VA_ARGS__)
 #define LOGW(...) __android_log_print(ANDROID_LOG_WARN,  LOG_TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 #define LOGF(...) __android_log_print(ANDROID_LOG_FATAL, LOG_TAG, __VA_ARGS__)
+#else
+#define LOGD(...)
+#define LOGI(...)
+#define LOGW(...)
+#define LOGE(...)
+#define LOGF(...)
+#endif
 
 #ifndef LOG_ROOT
 #define LOG_ROOT "/sdcard/"
@@ -20,7 +33,12 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+
+#if LOG_ON
 #define LOGI(...) printf(__VA_ARGS__)
+#else
+#define LOGI(...)
+#endif
 
 #ifndef LOG_ROOT
 #define LOG_ROOT "/tmp/"
@@ -29,3 +47,5 @@
 #endif
 
 #endif
+
+#endif // CG_LOGGING_H
